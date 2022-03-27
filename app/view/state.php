991c54304c938bb3cdad30e18357c4f3d5bd84ab
JaxonRailey <96697458+JaxonRailey@users.noreply.github.com>
2022-03-27 20:46:06 +0200

    Update state.php

--- app/view/state.php
+++ app/view/state.php
@@ -1,5 +1,5 @@
 <h1><?php echo $continent; ?></h1>
-<h4>Lista delle nazioni del continente <?php echo $continent; ?></h4>
+<h4>List of nations for the continent <?php echo $continent; ?></h4>
 
 <ul>
     <?php foreach ($states as $state) { ?>
